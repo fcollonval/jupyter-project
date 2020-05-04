9866ed9fd8cda96fa9c47d543663b47f0f1e076d
Frederic Collonval <fcollonval@gmail.com>
2020-05-04 10:18:07 +0200

    Don't use multi-packages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   // collectCoverageFrom: ['packages/*/src/*.{js,ts,tsx}'],
   // coverageReporters: ['html', 'lcovonly', 'text-summary'],
-  // moduleNameMapper: {
-  //   '^uniforms-jlab$': '<rootDir>/packages/uniforms-jlab/src',
-  //   '^uniforms([^/]*)(.*)$': '<rootDir>/node_modules/uniforms$1/$2'
-  // },
   setupFiles: ['./scripts/setupEnzyme.js'],
   testMatch: ['**/__tests__/**/!(_)*.{js,ts,tsx}'],
   transform: {
